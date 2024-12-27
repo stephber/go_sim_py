74f1f0f034c4a9d55e0f5fa74512587294871ce0
Kaisar Abutalipov <kaisar.abutalipov@enterar.com>
2024-12-27 16:15:05 +0500

    udrf with namespace modified

--- go1_description/launch/gazebo_multi_nav2_world.launch.py
+++ go1_description/launch/gazebo_multi_nav2_world.launch.py
@@ -1,9 +1,14 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, GroupAction, RegisterEventHandler
+from launch.actions import (
+    IncludeLaunchDescription,
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    GroupAction,
+    RegisterEventHandler
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
@@ -11,27 +16,19 @@ from launch_ros.actions import Node, SetRemap
 from launch.event_handlers import OnProcessExit
 import xacro, yaml
 
+
 def generate_launch_description():
     ld = LaunchDescription()
-
 
     package_name = 'go1_description'
     pkg_path = get_package_share_directory(package_name)
     robots_file_path = os.path.join(pkg_path, 'config', 'robots.yaml')
 
-    # Создаем пустой список роботов
-    robots = []
-
     # Загрузка данных из YAML файла
     with open(robots_file_path, 'r') as file:
         yaml_data = yaml.safe_load(file)
 
-    # Добавление роботов из YAML файла в список
-    robots.extend(yaml_data['robots'])
-
-
-
-    
+    robots = yaml_data['robots']
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     declare_use_sim_time = DeclareLaunchArgument(
@@ -45,24 +42,21 @@ def generate_launch_description():
         name='enable_rviz', default_value=enable_rviz, description='Enable rviz launch'
     )
 
-
     ld.add_action(declare_enable_rviz)
     ld.add_action(declare_use_sim_time)
 
     world_file = os.path.join(pkg_path, 'world', 'empty.world') 
     gazebo = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                launch_arguments={'gz_args': ['-r -v4 ', world_file], 'on_exit_shutdown': 'true'}.items()
-        )
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': ['-r -v4 ', world_file], 'on_exit_shutdown': 'true'}.items()
+    )
     ld.add_action(gazebo)
 
-    remappings=[
-            ("/tf", "tf"),
-            ("/tf_static", "tf_static"),
-            # ("/scan", "scan"),
-            # ("/odom", "odom")
-        ]    
+    remappings_initial = [
+        ("/tf", "tf"),
+        ("/tf_static", "tf_static"),
+    ]
     
     map_server = Node(package='nav2_map_server',
                       executable='map_server',
@@ -70,7 +64,7 @@ def generate_launch_description():
                       output='screen',
                       parameters=[{'yaml_filename': os.path.join(get_package_share_directory('go1_description'), 'maps', 'warehouse_map.yaml'),
                                    }, ],
-                      remappings=remappings)
+                      remappings=remappings_initial)
 
     map_server_lifecycle = Node(package='nav2_lifecycle_manager',
                                 executable='lifecycle_manager',
@@ -93,9 +87,8 @@ def generate_launch_description():
         ]    
     
     last_action = None
-    # Spawn turtlebot3 instances in gazebo
-    for robot in robots:
 
+    for i, robot in enumerate(robots):
         namespace = robot['name']
         robot_name = robot['name']
         xacro_file = os.path.join(pkg_path, 'xacro', 'robot.xacro')
@@ -111,20 +104,21 @@ def generate_launch_description():
             remappings=remappings
         )
 
+        spawn_entity = Node(
+            package='ros_gz_sim',
+            executable='create',
+            namespace=namespace,
+            arguments=[
+                '-topic', f'/{namespace}/robot_description',
+                '-name', f'{namespace}_my_bot',
+                '-allow_renaming', 'true',
+                '-x', robot['x_pose'],
+                '-y', robot['y_pose'],
+                '-z', robot['z_pose']
+            ],
+            output='screen'
+        )
 
-
-
-
-
-        spawn_entity = Node(package='ros_gz_sim', executable='create', namespace=namespace,
-                            arguments=['-topic', f'/{namespace}/robot_description',
-                                    '-name', f'/{namespace}/my_bot',
-                                    '-x', robot['x_pose'],
-                                    '-y', robot['y_pose'],
-                                    '-z', robot['z_pose']],
-                            output='screen')
-
-        
         ros_gz_bridge = Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
@@ -134,17 +128,16 @@ def generate_launch_description():
             arguments=[
                 f'/{namespace}/imu_plugin/out@sensor_msgs/msg/Imu@gz.msgs.IMU',
                 f'/{namespace}/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
-                # f'/{namespace}/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock',
                 f'/{namespace}/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
                 f'/{namespace}/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model'
             ]
         )
 
-
         joint_state_broadcaster = Node(
             package='controller_manager',
             executable='spawner',
             namespace=namespace,
+            name='joint_state_broadcaster',
             arguments=['joint_state_broadcaster'],
             output='screen',
             remappings=remappings
@@ -154,6 +147,7 @@ def generate_launch_description():
             package='controller_manager',
             executable='spawner',
             namespace=namespace,
+            name='joint_group_controller',
             arguments=['joint_group_controller'],
             output='screen',
             remappings=remappings
@@ -189,22 +183,21 @@ def generate_launch_description():
             remappings=remappings
         )
 
-
         nav2_launch_file = os.path.join(pkg_path, 'launch', 'nav2', 'bringup_launch.py')
-        map_yaml_file = os.path.join( get_package_share_directory(package_name), 'maps', 'warehouse_map.yaml')
+        map_yaml_file = os.path.join(get_package_share_directory(package_name), 'maps', 'warehouse_map.yaml')
         params_file = os.path.join(get_package_share_directory(package_name), 'config', 'nav2_params.yaml')
-        
+
         message = f"{{header: {{frame_id: map}}, pose: {{pose: {{position: {{x: {robot['x_pose']}, y: {robot['y_pose']}, z: 0.1}}, orientation: {{x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}}, }} }}"
 
         initial_pose_cmd = ExecuteProcess(
             cmd=[
-                'ros2', 'topic', 'pub', '-t', '3', '--qos-reliability', 'reliable', 
-                namespace + '/initialpose',
+                'ros2', 'topic', 'pub', '-t', '3', '--qos-reliability', 'reliable',
+                f'/{namespace}/initialpose',
                 'geometry_msgs/PoseWithCovarianceStamped', message
             ],
             output='screen'
-        )        
-        
+        )
+
         bringup_cmd = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(nav2_launch_file),
             launch_arguments={
@@ -226,9 +219,6 @@ def generate_launch_description():
             initial_pose_cmd,
         ])
 
-
-
-
         rviz_launch_file = os.path.join(pkg_path, 'launch', 'rviz_launch.py')
         rviz_config_file = os.path.join(pkg_path, 'config', 'multi_nav2_default_view.rviz')
 
@@ -242,7 +232,6 @@ def generate_launch_description():
             condition=IfCondition(enable_rviz)
         )
 
-
         cmd_vel_pub = Node(
             package='go1_controller',
             executable='cmd_vel_pub.py',
@@ -252,7 +241,6 @@ def generate_launch_description():
             remappings=remappings
         )
 
-        
         robot_control = GroupAction([
             SetRemap(src="/tf", dst="tf"),
             SetRemap(src="/tf_static", dst="tf_static"),
@@ -263,35 +251,31 @@ def generate_launch_description():
             odom
         ])
 
-        if last_action is None:
-            # Call add_action directly for the first robot to facilitate chain instantiation via RegisterEventHandler
-            ld.add_action(node_robot_state_publisher)
-            ld.add_action(spawn_entity)
-            ld.add_action(ros_gz_bridge)
-            ld.add_action(robot_control)
-            # ld.add_action(nav2_actions)
-            # ld.add_action(rviz)
 
+        # Группировка всех действий для робота
+        robot_group = GroupAction([
+            node_robot_state_publisher,
+            spawn_entity,
+            ros_gz_bridge,
+            robot_control,
+            # nav2_actions,
+            # rviz,
+        ])
+
+        if last_action is None:
+            # Первый робот запускается сразу
+            ld.add_action(robot_group)
         else:
-            # Use RegisterEventHandler to ensure next robot creation happens only after the previous one is completed.
-            # Simply calling ld.add_action for spawn_entity introduces issues due to parallel run.
+            # Последующие роботы запускаются после завершения spawn_entity предыдущего робота
             spawn_robot_event = RegisterEventHandler(
                 event_handler=OnProcessExit(
                     target_action=last_action,
-                    on_exit=[
-                        # rviz,
-                        # nav2_actions,
-                        robot_control,
-                        ros_gz_bridge,
-                        spawn_entity,
-                        node_robot_state_publisher,
-                    ],
+                    on_exit=[robot_group]
                 )
             )
-
             ld.add_action(spawn_robot_event)
 
-        # Save last instance for next RegisterEventHandler
+        # Сохраняем последний spawn_entity для следующего события
         last_action = spawn_entity
 
     return ld
