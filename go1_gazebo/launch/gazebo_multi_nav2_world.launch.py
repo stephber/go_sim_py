9a86bb18a32a9757d933b73df43d4819f66a8b50
Kaisar Abutalipov <kaisar.abutalipov@enterar.com>
2025-01-01 22:00:40 +0500

    update names

---
+++ go1_gazebo/launch/gazebo_multi_nav2_world.launch.py
@@ -0,0 +1,293 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    GroupAction,
+    RegisterEventHandler
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch_ros.actions import Node, SetRemap
+from launch.event_handlers import OnProcessExit
+import xacro, yaml
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    package_name = 'go1_gazebo'
+    pkg_path = get_package_share_directory(package_name)
+    robots_file_path = os.path.join(pkg_path, 'config', 'robots.yaml')
+
+    # Загрузка данных из YAML файла
+    with open(robots_file_path, 'r') as file:
+        yaml_data = yaml.safe_load(file)
+
+    robots = yaml_data['robots']
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    declare_use_sim_time = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='Использовать симуляционное время'
+    )
+
+    enable_rviz = LaunchConfiguration('enable_rviz', default='true')
+    declare_enable_rviz = DeclareLaunchArgument(
+        name='enable_rviz', default_value=enable_rviz, description='Enable rviz launch'
+    )
+
+    ld.add_action(declare_enable_rviz)
+    ld.add_action(declare_use_sim_time)
+
+    world_file = os.path.join(pkg_path, 'world', 'empty.world') 
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': ['-r -v4 ', world_file], 'on_exit_shutdown': 'true'}.items()
+    )
+    ld.add_action(gazebo)
+
+    remappings_initial = [
+        ("/tf", "tf"),
+        ("/tf_static", "tf_static"),
+        ("/scan", "scan"),
+        ("/odom", "odom")
+    ]
+    
+    map_server = Node(package='nav2_map_server',
+                      executable='map_server',
+                      name='map_server',
+                      output='screen',
+                      parameters=[{'yaml_filename': os.path.join(get_package_share_directory('go1_gazebo'), 'maps', 'warehouse_map.yaml'),
+                                   }, ],
+                      remappings=remappings_initial)
+
+    map_server_lifecycle = Node(package='nav2_lifecycle_manager',
+                                executable='lifecycle_manager',
+                                name='lifecycle_manager_map_server',
+                                output='screen',
+                                parameters=[{'use_sim_time': use_sim_time},
+                                            {'autostart': True},
+                                            {'node_names': ['map_server']}])
+
+    # ld.add_action(map_server)
+    # ld.add_action(map_server_lifecycle)
+
+
+    remappings=[
+            ("/tf", "tf"),
+            ("/tf_static", "tf_static"),
+            ("/scan", "scan"),
+            ("/odom", "odom")
+        ]
+    
+
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge_clock = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+    ld.add_action(ros_gz_bridge_clock)   
+
+    
+    last_action = None
+
+    for i, robot in enumerate(robots):
+        namespace = robot['name']
+        robot_name = robot['name']
+        xacro_file = os.path.join(pkg_path, 'xacro', 'robot.xacro')
+        robot_desc = xacro.process_file(xacro_file, mappings={'robot_name': robot_name}).toxml()
+        params_robot_state_publisher = {'robot_description': robot_desc, 'use_sim_time': use_sim_time}
+
+        node_robot_state_publisher = Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            namespace=namespace,
+            parameters=[params_robot_state_publisher],
+            remappings=remappings
+        )
+
+        spawn_entity = Node(
+            package='ros_gz_sim',
+            executable='create',
+            namespace=namespace,
+            arguments=[
+                '-topic', f'/{namespace}/robot_description',
+                '-name', f'{namespace}_my_bot',
+                '-allow_renaming', 'true',
+                '-x', robot['x_pose'],
+                '-y', robot['y_pose'],
+                '-z', robot['z_pose']
+            ],
+            output='screen'
+        )
+
+        ros_gz_bridge = Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            namespace=namespace,
+            name='ros_gz_bridge',
+            output='screen',
+            arguments=[
+                f'/{namespace}/imu_plugin/out@sensor_msgs/msg/Imu@gz.msgs.IMU',
+                f'/{namespace}/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                f'/{namespace}/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
+                f'/{namespace}/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model'
+            ]
+        )
+
+        joint_state_broadcaster = Node(
+            package='controller_manager',
+            executable='spawner',
+            namespace=namespace,
+            name='joint_state_broadcaster',
+            arguments=['joint_state_broadcaster'],
+            output='screen',
+            remappings=remappings
+        )
+
+        joint_group_controller = Node(
+            package='controller_manager',
+            executable='spawner',
+            namespace=namespace,
+            name='joint_group_controller',
+            arguments=['joint_group_controller'],
+            output='screen',
+            remappings=remappings
+        )
+
+        controller = Node(
+            package='quadropted_controller',
+            executable='robot_controller_gazebo.py',
+            name='quadruped_controller',
+            namespace=namespace,
+            output='screen',
+            remappings=remappings
+        )
+
+        odom = Node(
+            package='quadropted_controller',
+            executable='QuadrupedOdometryNode.py',
+            name='odom',
+            namespace=namespace,
+            output='screen',
+            parameters=[{
+                "verbose": False,
+                'publish_rate': 50,
+                'open_loop': False,
+                'has_imu_heading': True,
+                'is_gazebo': True,
+                'imu_topic': f'/{namespace}/imu',
+                'base_frame_id': "base",
+                'odom_frame_id': "odom",
+                'clock_topic': f'/clock',
+                'enable_odom_tf': True,
+            }],
+            remappings=remappings
+        )
+
+        nav2_launch_file = os.path.join(pkg_path, 'launch', 'nav2', 'bringup_launch.py')
+        map_yaml_file = os.path.join(get_package_share_directory(package_name), 'maps', 'warehouse_map.yaml')
+        params_file = os.path.join(get_package_share_directory(package_name), 'config', 'nav2_params.yaml')
+
+        message = f"{{header: {{frame_id: map}}, pose: {{pose: {{position: {{x: {robot['x_pose']}, y: {robot['y_pose']}, z: 0.1}}, orientation: {{x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}}, }} }}"
+
+        initial_pose_cmd = ExecuteProcess(
+            cmd=[
+                'ros2', 'topic', 'pub', '-t', '3', '--qos-reliability', 'reliable',
+                f'/{namespace}/initialpose',
+                'geometry_msgs/PoseWithCovarianceStamped', message
+            ],
+            output='screen'
+        )
+
+        bringup_cmd = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch_file),
+            launch_arguments={
+                'map': map_yaml_file,
+                'use_namespace': 'True',
+                'namespace': namespace,
+                'params_file': params_file,  
+                'autostart': 'true',
+                'use_sim_time': 'true',
+                'log_level': 'warn',
+                'map_server': 'True'
+            }.items()
+        )
+
+        nav2_actions = GroupAction([
+            SetRemap(src="/tf", dst="tf"),
+            SetRemap(src="/tf_static", dst="tf_static"),
+            bringup_cmd,
+            initial_pose_cmd,
+        ])
+
+        rviz_launch_file = os.path.join(pkg_path, 'launch', 'rviz_launch.py')
+        rviz_config_file = os.path.join(pkg_path, 'config', 'multi_nav2_default_view.rviz')
+
+        rviz = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rviz_launch_file),
+            launch_arguments={
+                "namespace": namespace,
+                "use_namespace": 'true',
+                "rviz_config": rviz_config_file,
+            }.items(),
+            condition=IfCondition(enable_rviz)
+        )
+
+        cmd_vel_pub = Node(
+            package='quadropted_controller',
+            executable='cmd_vel_pub.py',
+            namespace=namespace,
+            name='cmd_vel_pub',
+            output='screen',
+            remappings=remappings
+        )
+
+        robot_control = GroupAction([
+            SetRemap(src="/tf", dst="tf"),
+            SetRemap(src="/tf_static", dst="tf_static"),
+            joint_state_broadcaster,
+            joint_group_controller,
+            controller,
+            cmd_vel_pub,
+            odom
+        ])
+
+
+        # Группировка всех действий для робота
+        robot_group = GroupAction([
+            node_robot_state_publisher,
+            spawn_entity,
+            ros_gz_bridge,
+            robot_control,
+            nav2_actions,
+            rviz,
+        ])
+
+        if last_action is None:
+            ld.add_action(robot_group)
+        else:
+            spawn_robot_event = RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=last_action,
+                    on_exit=[robot_group]
+                )
+            )
+            ld.add_action(spawn_robot_event)
+
+        last_action = joint_group_controller
+
+    return ld
---
+++ quadropted_controller/scripts/cmd_vel_pub.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from quadropted_msgs.msg import RobotVelocity
+
+class RobotVelocityHandler(Node):
+    def __init__(self):
+        super().__init__('robot_velocity_handler')
+
+        self.subscription = self.create_subscription(
+            Twist, 
+            'cmd_vel',
+            self.robot_velocity_callback, 
+            10)
+
+        self.publisher_ = self.create_publisher(RobotVelocity, 'robot_velocity', 10)
+
+        self.get_logger().info("Node started: RobotVelocityHandler")
+
+    def robot_velocity_callback(self, msg: Twist):
+        self.get_logger().info(
+            f"Received Twist: linear=({msg.linear.x}, {msg.linear.y}, {msg.linear.z}), "
+            f"angular=({msg.angular.x}, {msg.angular.y}, {msg.angular.z})"
+        )
+
+        new_msg = RobotVelocity()
+
+        new_msg.robot_id = 1
+
+        new_msg.cmd_vel.linear.x = self.multiply_and_limit(msg.linear.x, 0.035, -1.0, 1.0)
+        new_msg.cmd_vel.linear.y = self.multiply_and_limit(msg.linear.y, 0.012, -1.0, 1.0)
+        new_msg.cmd_vel.linear.z = msg.linear.z
+
+        new_msg.cmd_vel.angular.x = msg.angular.x
+        new_msg.cmd_vel.angular.y = msg.angular.y
+        new_msg.cmd_vel.angular.z = self.limit(msg.angular.z, -0.5, 0.5)
+
+        self.publisher_.publish(new_msg)
+
+        self.get_logger().info(
+            f"Published RobotVelocity: robot_id={new_msg.robot_id}, "
+            f"linear=({new_msg.cmd_vel.linear.x}, {new_msg.cmd_vel.linear.y}, {new_msg.cmd_vel.linear.z}), "
+            f"angular=({new_msg.cmd_vel.angular.x}, {new_msg.cmd_vel.angular.y}, {new_msg.cmd_vel.angular.z})"
+        )
+        
+    def multiply_and_limit(self, value, scale_factor, min_limit, max_limit):
+        scaled_value = value * scale_factor
+        return self.limit_value(scaled_value, min_limit, max_limit)
+
+    def limit_value(self, value, min_limit, max_limit):
+        if value > max_limit:
+            return max_limit
+        elif value < min_limit:
+            return min_limit
+        else:
+            return value
+
+    def limit(self, value, min_limit, max_limit):
+        if value > max_limit:
+            return max_limit
+        elif value < min_limit:
+            return min_limit
+        else:
+            return value
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = RobotVelocityHandler()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
